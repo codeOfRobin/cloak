bb6302842b542a6590e83072fcb407eb89337f0c
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-07-05 00:00:57 +0530

    Initial Commit

---
+++ cloak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cloak
+//
+//  Created by Robin Malhotra on 05/07/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
