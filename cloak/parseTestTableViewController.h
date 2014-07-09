f4c8cf13d8fca08c5dd894681fc4f4d4de23d21c
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-07-09 20:09:31 +0530

    added a cloud code function that I need to test. @Anurag, we really need to discuss how this is going to work

---
+++ cloak/parseTestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  parseTestTableViewController.h
+//  cloak
+//
+//  Created by Robin Malhotra on 09/07/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface parseTestTableViewController : UITableViewController
+@property (nonatomic,strong) NSArray *posts;
+@end
